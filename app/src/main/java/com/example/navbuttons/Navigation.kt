0903694cb44a8929ec6a2d755f1ff02baa0c1a31
newandru-sudo <newandru@gmail.com>
2026-05-22 21:41:29 +0500

    Add app/src/main/java/com/example/navbuttons/Navigation.kt

---
+++ app/src/main/java/com/example/navbuttons/Navigation.kt
@@ -0,0 +1,24 @@
+package com.example.navbuttons
+
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.entryProvider
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.ui.NavDisplay
+import com.example.navbuttons.ui.main.MainScreen
+
+@Composable
+fun MainNavigation() {
+    val backStack = rememberNavBackStack(Main)
+
+    NavDisplay(
+        backStack = backStack,
+        onBack    = { backStack.removeLastOrNull() },
+        entryProvider = entryProvider {
+            entry<Main> {
+                MainScreen(modifier = Modifier.safeDrawingPadding())
+            }
+        },
+    )
+}
