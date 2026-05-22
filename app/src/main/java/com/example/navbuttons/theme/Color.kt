6b25ead4c65660dcc7900b3399ce27c57a5a488f
newandru-sudo <newandru@gmail.com>
2026-05-22 21:41:34 +0500

    Add app/src/main/java/com/example/navbuttons/theme/Color.kt

---
+++ app/src/main/java/com/example/navbuttons/theme/Color.kt
@@ -0,0 +1,26 @@
+package com.example.navbuttons.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Dark blue background palette
+val DeepNavy        = Color(0xFF050A18)
+val DarkNavy        = Color(0xFF0A1128)
+val NavyBlue        = Color(0xFF0D1B3E)
+val MediumNavy      = Color(0xFF112255)
+
+// Neon / glow colors
+val NeonBlue        = Color(0xFF1E90FF)
+val NeonBlueBright  = Color(0xFF4DB8FF)
+val NeonBlueDim     = Color(0xFF0A4A8A)
+val GlowBlue        = Color(0x991E90FF)   // semi-transparent glow layer
+val GlowBlueSoft    = Color(0x441E90FF)   // very subtle ambient
+
+// Text
+val IceWhite        = Color(0xFFE8F4FF)
+val SoftWhite       = Color(0xFFB0C4DE)
+val DimBlue         = Color(0xFF5A7FA8)
+
+// Surface
+val CardSurface     = Color(0xFF0C1630)
+val CardBorder      = Color(0xFF1A2E5A)
+val PressedSurface  = Color(0xFF0F1F45)
